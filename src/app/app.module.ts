c33caff8fbc77af9dece0745a09aa350ba897ef0
John Dowd <john.dowd@inspectall.com>
2017-07-14 14:25:31 -0700

    add edit-profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AboutComponent } from './about/about.component';
 import { MembersComponent } from './members/members.component';
 import { ProfileDetailComponent } from './profile-detail/profile-detail.component';
 import { AdminComponent } from './admin/admin.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
@@ -28,7 +29,8 @@ export const firebaseConfig = {
     AboutComponent,
     MembersComponent,
     ProfileDetailComponent,
-    AdminComponent
+    AdminComponent,
+    EditProfileComponent
   ],
   imports: [
     BrowserModule,
