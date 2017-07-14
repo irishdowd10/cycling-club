cd611fbc0fefbe30214ac59a433e5b55aa9fc693
John Dowd <john.dowd@inspectall.com>
2017-07-14 09:08:56 -0700

    add nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'North Bethany Cycling Club';
+  title = 'Are you Riding or Hiding?';
 }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,7 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
 
 
 
@@ -8,7 +9,11 @@ const appRoutes: Routes = [
   {
     path: '',
     component: WelcomeComponent
-  }
+  },
+  {
+  path: 'about',
+  component: AboutComponent
+}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
